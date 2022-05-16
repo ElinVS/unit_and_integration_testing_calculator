e97e3dfd136cd2718bab960a0860bf45bdc29538
ElinVS <elinvsvennberg@gmail.com>
2022-05-16 19:53:40 +0100

    tidied up a file

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -77,7 +77,6 @@ describe("Calculator", () => {
     cy.get('#number5').click()
     cy.get('#operator-multiply').click()
     cy.get('#number2').click()
-
     cy.get('#operator-equals').click()
 
     cy.get('.display').should('contain', '17')
@@ -99,7 +98,6 @@ describe("Calculator", () => {
   cy.get('#number4').click()
   cy.get('#number5').click()
   cy.get('#number6').click()
-
   cy.get('#operator-equals').click()
 
   cy.get('.display').should('contain', '27434639232')
@@ -117,15 +115,4 @@ it('should display error when a number is divided with 0', () => {
 
 })
 
-
-
 })
-
-
-// DONE Do the number buttons update the display of the running total?
-// DONE Do the arithmetical operations update the display with the result of the operation?
-// DONE Can multiple operations be chained together?
-// Is the output as expected for a range of numbers (for example, positive, negative, decimals and very large numbers)?
-
-
-// What does the code do in exceptional circumstances? Specifically, if you divide by zero, what is the effect? Write a test to describe what you'd prefer to happen, and then correct the code to make that test pass (you will need to modify the Calculator model to meet this requirement).
